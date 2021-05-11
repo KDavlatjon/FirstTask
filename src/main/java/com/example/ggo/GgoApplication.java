849d2c97bdb196a8ec2d838ccc32ff8683a78978
KDavlatjon <79334228+KDavlatjon@users.noreply.github.com>
2021-05-11 15:59:28 +0500

    Spring 1-commit

---
+++ src/main/java/com/example/ggo/GgoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ggo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ggo/GgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ggo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
